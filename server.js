94edc3e85d02724a87f9afcc585f89333e7c5ba4
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-19 18:46:42 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,22 +6,27 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+// MIDDLEWARE
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads");
+});
 
 // Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
-
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // LISTEN
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
+});
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
 });
