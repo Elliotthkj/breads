59326924c069529dc8bd488a4787ff676a2d1f83
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-19 18:59:20 -0700

    added 404 page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,15 +11,16 @@ breads.get("/", (req, res) => {
 });
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
+breads.get("/:arrayIndex", (req, res) => {
   if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
-    })
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
   } else {
-    res.send('404')
+    res.render("404");
   }
-})
+});
+
 
 
 module.exports = breads;
--- views/404.jsx
+++ views/404.jsx
@@ -2,11 +2,21 @@ const React = require("react");
 const Default = require("./layouts/default");
 
 function index({ breads, title }) {
+  if (!breads || !breads.length) {
+    return (
+      <Default title="404 Not Found">
+        <h1>404 Not Found</h1>
+        <p>The bread you requested could not be found. :(</p>
+        <p>
+          <a href="/breads">Back to Breads</a>
+        </p>
+      </Default>
+    );
+  }
+
   return (
     <Default title={title}>
       <h2>index Page</h2>
-      {/* <p>I have {breads[0].name} bread!</p> */}
-      {/* This is a JSX comment. */}
       <ul>
         {breads.map((bread, index) => {
           return (
