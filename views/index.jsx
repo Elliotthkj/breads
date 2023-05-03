07f173e6b4b2e56c88d9a1331bf06649bdfb1100
Elliott Johnson <elliotthkj1991@gmail.com>
2023-05-03 14:01:23 -0700

    created virtual to find all related children documents on a parent

--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,21 @@
 const React = require("react");
 const Default = require("./layouts/default");
 
-function index({ breads, title }) {
+function index({ breads, bakers, title }) {
   return (
     <Default title={title}>
       <h2>index Page</h2>
+      <h3>Bakers</h3>
+      <ul>
+        {bakers.map((baker) => {
+          return (
+            <li key={baker.id}>
+              <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <h3>Breads</h3>
       {/* This is a JSX comment. */}
       <ul>
         {breads.map((bread, index) => {
